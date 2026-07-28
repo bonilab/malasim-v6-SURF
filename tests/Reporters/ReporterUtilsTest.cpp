3eb60af281233db7423cf66ceb385166d90967e1
Nguyen Tran <merlinvn@gmail.com>
2026-07-28 14:41:07 +0700

    test: cover reporter genotype frequency paths

--- tests/Reporters/ReporterUtilsTest.cpp
+++ tests/Reporters/ReporterUtilsTest.cpp
@@ -57,3 +57,43 @@ TEST_F(ReporterUtilsModelTest, EmptyPopulationSupportsMosquitoAndMoiReports) {
   EXPECT_NO_THROW(ReporterUtils::output_moi(moi, &person_index));
   EXPECT_TRUE(moi.str().empty());
 }
+
+TEST_F(ReporterUtilsModelTest, ReportsGenotypeFrequenciesForAnInfectedPerson) {
+  auto *pi = Model::get_population()->get_person_index<PersonIndexByLocationStateAgeClass>();
+  Person *infected = nullptr;
+  for (auto &location : pi->vPerson()) {
+    for (auto state = 0; state < Person::NUMBER_OF_STATE - 1 && infected == nullptr; ++state) {
+      for (auto &age_class : location[state]) {
+        if (!age_class.empty()) {
+          infected = age_class.front();
+          break;
+        }
+      }
+    }
+    if (infected != nullptr) break;
+  }
+  ASSERT_NE(infected, nullptr);
+  infected->add_new_parasite_to_blood(Model::get_genotype_db()->at(0));
+
+  std::stringstream frequency1;
+  std::stringstream frequency2;
+  std::stringstream frequency3;
+  std::stringstream frequency4;
+  std::stringstream prmc;
+  std::stringstream combined;
+  std::stringstream moi;
+
+  ReporterUtils::output_genotype_frequency1(frequency1, 2, pi);
+  ReporterUtils::output_genotype_frequency2(frequency2, 2, pi);
+  ReporterUtils::output_genotype_frequency3(frequency3, 2, pi);
+  ReporterUtils::output_genotype_frequency4(frequency4, prmc, 2, pi);
+  ReporterUtils::output_3_genotype_frequency(combined, 2, pi);
+  ReporterUtils::output_moi(moi, pi);
+
+  EXPECT_FALSE(frequency1.str().empty());
+  EXPECT_FALSE(frequency2.str().empty());
+  EXPECT_FALSE(frequency3.str().empty());
+  EXPECT_FALSE(frequency4.str().empty());
+  EXPECT_FALSE(combined.str().empty());
+  EXPECT_TRUE(moi.str().empty());
+}
